1f9b1eb5ac2927eb466c8fdb3202542a06cce6c6
Oleg Ilyenko <oleg.ilyenko@gmail.com>
2013-11-12 12:07:54 +0100

    replaced fmpp with sbt-boilerplate

--- project/Build.scala
+++ project/Build.scala
@@ -48,41 +48,6 @@ object SphereLibsBuild extends Build {
     id = "sphere-json",
     base = file("./json"),
     //dependencies = Seq(util),
-    settings = standardSettings ++ Fmpp.settings //++ Seq(scalacOptions ++= Seq("-Ymacro-debug-lite"))
+    settings = standardSettings ++ spray.boilerplate.BoilerplatePlugin.Boilerplate.settings //++ Seq(scalacOptions ++= Seq("-Ymacro-debug-lite"))
   )
-}
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// Code generation via FMPP
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-object Fmpp {
-  private lazy val fmpp = TaskKey[Seq[File]]("fmpp")
-  private lazy val fmppOptions = SettingKey[Seq[String]]("fmpp-options")
-  private lazy val fmppConfig = config("fmpp") hide
-
-  lazy val settings = fmppConfig(Compile) ++ Seq(
-    libraryDependencies += "net.sourceforge.fmpp" % "fmpp" % "0.9.14" % fmppConfig.name,
-    ivyConfigurations += fmppConfig,
-    fmppOptions := "--ignore-temporary-files" :: Nil,
-    fullClasspath in fmppConfig <<= update map { _ select configurationFilter(fmppConfig.name) map Attributed.blank }
-  )
-
-  private def fmppConfig(c: Configuration): Seq[Setting[_]] = inConfig(c)(Seq(
-    sourceGenerators in Compile <+= fmpp,
-    fmpp <<= fmppTask,
-    scalaSource <<= (baseDirectory, configuration) { (base, c) => base / (Defaults.prefix(c.name) + "src") },
-    sources <<= managedSources
-  ))
-
-  private val fmppTask =
-    (fullClasspath in fmppConfig, runner in fmpp, unmanagedSources, scalaSource, sourceManaged, fmppOptions, streams) map {
-      (cp, r, sources, srcRoot, output, args, s) => {
-        IO.delete(output)
-        val arguments = "-U" +: "all" +: "-S" +: srcRoot.getAbsolutePath +: "-O" +: output.getAbsolutePath +:
-          "-M" +: "ignore(test/**,it/**),execute(**/*.fmpp.scala),copy(**/*)" +: Seq.empty
-        toError(r.run("fmpp.tools.CommandLine", cp.files, arguments, s.log))
-        (output ** "*.scala").get ++ (output ** "*.java").get
-      }
-    }
 }
