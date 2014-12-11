c0e15805877c8d1ca558be155705c456acf6162b
Oleg Ilyenko <oleg.ilyenko@gmail.com>
2014-12-11 14:20:51 +0100

    added bintray plugin

--- project/Build.scala
+++ project/Build.scala
@@ -2,13 +2,18 @@ import sbt._
 import Keys._
 import language._
 import sbtrelease.ReleasePlugin._
+import bintray.Plugin._
 
 object SphereLibsBuild extends Build {
 
-  lazy val standardSettings = Defaults.defaultSettings ++ releaseSettings ++ Seq(
+  lazy val publishSettings = releaseSettings ++ bintraySettings
+
+  lazy val standardSettings = Defaults.defaultSettings ++ bintrayResolverSettings ++ Seq(
     organization := "io.sphere",
     scalaVersion := "2.10.4",
+    licenses += ("Apache-2.0", url("https://www.apache.org/licenses/LICENSE-2.0.html")),
     logBuffered := false,
+    publishMavenStyle := false,
     publishTo := Some("bintray-public" at "https://api.bintray.com/maven/commercetools/maven/sphere-libs"),
     resolvers += "sphere-public" at "http://dl.bintray.com/commercetools/maven",
     credentials ++= Seq(Credentials(Path.userHome / ".bintray-credentials")),
@@ -22,26 +27,26 @@ object SphereLibsBuild extends Build {
       "org.scalacheck" %% "scalacheck" % "1.12.1" % "test",
       "ch.qos.logback" % "logback-classic" % "1.0.6" % "test"
     ),
-    libraryDependencies <+= scalaVersion("org.scala-lang" % "scalap" % _)
+    libraryDependencies += "org.scala-lang" % "scalap" % scalaVersion.value
   )
 
   lazy val libs = Project(
     id = "sphere-libs",
     base = file("."),
-    settings = standardSettings,
-    aggregate = Seq(util, json)
+    aggregate = Seq(util, json),
+    settings = standardSettings
   )
 
   lazy val util = Project(
     id = "sphere-util",
     base = file("./util"),
-    settings = standardSettings
+    settings = standardSettings ++ publishSettings
   )
 
   lazy val json = Project(
     id = "sphere-json",
     base = file("./json"),
-    settings = standardSettings ++ Fmpp.settings
+    settings = standardSettings ++ publishSettings ++ Fmpp.settings
   )
 }
 
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,1 +1,4 @@
+resolvers += Resolver.url("bintray-sbt-plugin-releases", url("http://dl.bintray.com/content/sbt/sbt-plugin-releases"))(Resolver.ivyStylePatterns)
+
+addSbtPlugin("me.lessis" % "bintray-sbt" % "0.1.2")
 addSbtPlugin("com.github.gseitz" % "sbt-release" % "0.8.5")
