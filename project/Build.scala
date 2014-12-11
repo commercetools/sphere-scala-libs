64a119ed818ecaa87515c9cfbb504cab44690bc8
Oleg Ilyenko <oleg.ilyenko@gmail.com>
2014-12-11 14:26:12 +0100

    fixed merge issue

--- project/Build.scala
+++ project/Build.scala
@@ -21,8 +21,7 @@ object SphereLibsBuild extends Build {
     javacOptions ++= Seq("-deprecation", "-Xlint:unchecked"),
     testOptions in Test := Seq(
       Tests.Argument(TestFrameworks.ScalaTest, "-oDF"),
-      Tests.Argument(TestFrameworks.ScalaTest, "-u", "%s" format (target / "test-reports")))
-    },
+      Tests.Argument(TestFrameworks.ScalaTest, "-u", "%s" format (target / "test-reports"))),
     libraryDependencies ++= Seq(
       "org.scalatest" %% "scalatest" % "2.2.2" % "test",
       "org.scalacheck" %% "scalacheck" % "1.12.1" % "test",
