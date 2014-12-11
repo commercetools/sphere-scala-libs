b6e6fe009c84f92b9d0ecc4ae1b9bd5cde99d536
Arnaud Gourlay <arnaud.gourlay@commercetools.de>
2014-12-11 13:36:53 +0100

    use LazyLogging

--- util/src/main/scala/Logging.scala
+++ util/src/main/scala/Logging.scala
@@ -1,5 +1,5 @@
 package io.sphere.util
 
-trait Logging extends com.typesafe.scalalogging.slf4j.StrictLogging {
+trait Logging extends com.typesafe.scalalogging.slf4j.LazyLogging {
   val log = logger
 }
