06b2691b70393d934eeba8a161b2e505bfad7aa5
Arnaud Gourlay <arnaud.gourlay@commercetools.de>
2015-03-05 12:43:51 +0100

    Scala 2.10.5 & Scala 2.11.6

--- project/Build.scala
+++ project/Build.scala
@@ -14,8 +14,8 @@ object SphereLibsBuild extends Build {
   lazy val standardSettings = Defaults.defaultSettings ++ publishSettings ++ Seq(
     organization := "io.sphere",
     licenses := Seq("Apache License, ASL Version 2.0" -> url("http://www.apache.org/licenses/LICENSE-2.0")),
-    scalaVersion := "2.10.4",
-    crossScalaVersions := Seq("2.10.4", "2.11.5"),
+    scalaVersion := "2.10.5",
+    crossScalaVersions := Seq("2.10.5", "2.11.6"),
     licenses += ("Apache-2.0", url("https://www.apache.org/licenses/LICENSE-2.0.html")),
     logBuffered := false,
     scalacOptions ++= Seq("-deprecation", "-unchecked", "-feature"),
