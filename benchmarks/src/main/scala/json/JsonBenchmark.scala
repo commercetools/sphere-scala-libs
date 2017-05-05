4c1ee4cc46755ab41ee887f8938a7231db51ee47
Yann Simon <yann.simon@commercetools.de>
2017-05-05 12:49:45 +0200

    update perf results

--- benchmarks/src/main/scala/json/JsonBenchmark.scala
+++ benchmarks/src/main/scala/json/JsonBenchmark.scala
@@ -10,9 +10,9 @@ class JsonBenchmark {
   /* on local mac
   jmh:run -i 10 -wi 10 -f1 -t1
 [info] Benchmark                       Mode  Cnt   Score   Error  Units
-[info] JsonBenchmark.parseFromString  thrpt   10  60.260 ± 1.812  ops/s
-[info] JsonBenchmark.listReader       thrpt   10  11.992 ± 1.499  ops/s
-[info] JsonBenchmark.vectorReader     thrpt   10   9.923 ± 1.029  ops/s
+[info] JsonBenchmark.parseFromString  thrpt   10  67.278 ± 3.246  ops/s
+[info] JsonBenchmark.listReader       thrpt   10  33.854 ± 2.066  ops/s
+[info] JsonBenchmark.vectorReader     thrpt   10  27.585 ± 2.733  ops/s
    */
 
   @Benchmark
