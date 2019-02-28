8833568e3a6f0fd00114a27c4a1607e320cc7ed4
Yann Simon <yann.simon@commercetools.de>
2019-02-28 09:52:19 +0100

    test MongoFormat[Option[_]]

---
+++ mongo/src/test/scala/io/sphere/mongo/format/OptionMongoFormatSpec.scala
@@ -0,0 +1,104 @@
+package io.sphere.mongo.format
+
+import io.sphere.mongo.generic._
+import org.scalatest.{MustMatchers, OptionValues, WordSpec}
+import io.sphere.mongo.MongoUtils._
+import DefaultMongoFormats._
+
+object OptionMongoFormatSpec {
+
+  case class SimpleClass(value1: String, value2: Int)
+
+  object SimpleClass {
+    implicit val mongo: MongoFormat[SimpleClass] = mongoProduct(apply _)
+  }
+
+  case class ComplexClass(
+    name: String,
+    simpleClass: Option[SimpleClass])
+
+  object ComplexClass {
+    implicit val mongo: MongoFormat[ComplexClass] = mongoProduct(apply _)
+  }
+
+}
+
+
+class OptionMongoFormatSpec extends WordSpec with MustMatchers with OptionValues {
+  import OptionMongoFormatSpec._
+
+  "MongoFormat[Option[_]]" should {
+    "handle presence of all fields" in {
+      val dbo = dbObj(
+        "value1" -> "a",
+        "value2" -> 45
+      )
+      val result = MongoFormat[Option[SimpleClass]].fromMongoValue(dbo)
+      result.value.value1 mustEqual "a"
+      result.value.value2 mustEqual 45
+    }
+
+    "handle presence of all fields mixed with ignored fields" in {
+      val dbo = dbObj(
+        "value1" -> "a",
+        "value2" -> 45,
+        "value3" -> "b"
+      )
+      val json =
+        """{
+          |  "value1": "a",
+          |  "value2": 45,
+          |  "value3": "b"
+          |}
+        """.stripMargin
+      val result = MongoFormat[Option[SimpleClass]].fromMongoValue(dbo)
+      result.value.value1 mustEqual "a"
+      result.value.value2 mustEqual 45
+    }
+
+    "handle presence of not all the fields" in {
+      val dbo = dbObj("value1" -> "a")
+      an[Exception] mustBe thrownBy (MongoFormat[Option[SimpleClass]].fromMongoValue(dbo))
+    }
+
+    "handle absence of all fields" in {
+      val dbo = dbObj()
+      val result = MongoFormat[Option[SimpleClass]].fromMongoValue(dbo)
+      result mustEqual None
+    }
+
+    "handle absence of all fields mixed with ignored fields" in {
+      val dbo = dbObj("value3" -> "a")
+      val result = MongoFormat[Option[SimpleClass]].fromMongoValue(dbo)
+      result mustEqual None
+    }
+
+    "do not ignore fields if no one is expected" in {
+      val dbo = dbObj(
+        "key1" -> "value1",
+        "key2" -> "value2"
+      )
+      val expected = Map("key1" → "value1", "key2" → "value2")
+      val result = MongoFormat[Map[String, String]].fromMongoValue(dbo)
+      result mustEqual expected
+
+      val maybeResult = MongoFormat[Option[Map[String, String]]].fromMongoValue(dbo)
+      maybeResult.value mustEqual expected
+    }
+
+    "parse optional element" in {
+      val dbo = dbObj(
+        "name" -> "ze name",
+        "simpleClass" -> dbObj(
+          "value1" -> "value1",
+          "value2" -> 42
+        )
+      )
+      val result = MongoFormat[ComplexClass].fromMongoValue(dbo)
+      result.simpleClass.value.value1 mustEqual "value1"
+      result.simpleClass.value.value2 mustEqual 42
+
+      MongoFormat[ComplexClass].toMongoValue(result) mustEqual dbo
+    }
+  }
+}
