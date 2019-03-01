e5196779032c405b5e09d1119a07c8421d431582
Yann Simon <yann.simon@commercetools.de>
2019-03-01 13:25:37 +0100

    Test deriving with default values

---
+++ mongo/src/test/scala/io/sphere/mongo/generic/DefaultValuesSpec.scala
@@ -0,0 +1,33 @@
+package io.sphere.mongo.generic
+
+import io.sphere.mongo.MongoUtils._
+import io.sphere.mongo.format.DefaultMongoFormats._
+import io.sphere.mongo.format.MongoFormat
+import org.scalatest.{MustMatchers, WordSpec}
+
+class DefaultValuesSpec extends WordSpec with MustMatchers {
+  import DefaultValuesSpec._
+
+  "deriving MongoFormat" must {
+    "handle default values" in {
+      val dbo = dbObj()
+      val test = MongoFormat[Test].fromMongoValue(dbo)
+      test.value1 must be ("hello")
+      test.value2 must be (None)
+      test.value3 must be (None)
+      test.value4 must be (Some("hi"))
+    }
+  }
+}
+
+object DefaultValuesSpec {
+  case class Test(
+    value1: String = "hello",
+    value2: Option[String],
+    value3: Option[String] = None,
+    value4: Option[String] = Some("hi")
+  )
+  object Test {
+    implicit val mongo: MongoFormat[Test] = mongoProduct(apply _)
+  }
+}
