2ca69b3763fff3a17ac5548c8483d9a18128c53b
Yann Simon <yann.simon@commercetools.de>
2019-03-01 23:15:28 +0100

    cats instances
    Fix https://github.com/sphereio/sphere-scala-libs/issues/83

---
+++ mongo/src/test/scala/io/sphere/mongo/catsinstances/MongoFormatCatsInstancesTest.scala
@@ -0,0 +1,27 @@
+package io.sphere.mongo.catsinstances
+
+import cats.syntax.invariant._
+import io.sphere.mongo.format.DefaultMongoFormats._
+import io.sphere.mongo.format._
+import org.scalatest.{MustMatchers, WordSpec}
+
+class MongoFormatCatsInstancesTest extends WordSpec with MustMatchers {
+  import MongoFormatCatsInstancesTest._
+
+  "Invariant[MongoFormat]" must {
+    "allow imaping a default format" in {
+      val myId = MyId("test")
+      val dbo = toMongo(myId)
+      dbo.asInstanceOf[String] must be ("test")
+      val myNewId = fromMongo[MyId](dbo)
+      myNewId must be (myId)
+    }
+  }
+}
+
+object MongoFormatCatsInstancesTest {
+  case class MyId(id: String) extends AnyVal
+  object MyId {
+    implicit val mongo: MongoFormat[MyId] = MongoFormat[String].imap(MyId.apply)(_.id)
+  }
+}
