f678ab264057db8210ac3ab9f9ed6c9b4d96adda
mladen <mladens42@gmail.com>
2022-03-11 17:38:33 +0100

    TypesSwitchSpec WIP

--- json/json-derivation-magnolia/src/test/scala/io/sphere/json/TypesSwitchSpec.scala
+++ json/json-derivation-magnolia/src/test/scala/io/sphere/json/TypesSwitchSpec.scala
@@ -1,7 +1,6 @@
-/*
 package io.sphere.json
 
-import io.sphere.json.generic.{TypeSelectorContainer, deriveJSON, jsonTypeSwitch}
+import io.sphere.json.generic.JSONMagnoliaDerivation.deriveJSON
 import org.json4s._
 import org.scalatest.matchers.must.Matchers
 import org.scalatest.wordspec.AnyWordSpec
@@ -33,7 +32,7 @@ class TypesSwitchSpec extends AnyWordSpec with Matchers {
     }
   }
 
-  "TypeSelectorContainer" must {
+  /*  "TypeSelectorContainer" must {
     "have information about type value discriminators" in {
       val selectors = Message.json.typeSelectors
       selectors.map(_.typeValue) must contain.allOf(
@@ -57,19 +56,18 @@ class TypesSwitchSpec extends AnyWordSpec with Matchers {
         "io.sphere.json.TypesSwitchSpec$TypeB"
       )
     }
-  }
-
+  }*/
 }
 
 object TypesSwitchSpec {
 
-  trait Message
+  // TODO must seal the trait
+  sealed trait Message
   object Message {
     // this can be dangerous is the same class name is used in both sum types
     // ex if we define TypeA.Class1 && TypeB.Class1
     // as both will use the same type value discriminator
-    implicit val json: JSON[Message] with TypeSelectorContainer =
-      jsonTypeSwitch[Message, TypeA, TypeB](Nil)
+    implicit val json: JSON[Message] = deriveJSON[Message]
   }
 
   sealed trait TypeA extends Message
@@ -86,4 +84,3 @@ object TypesSwitchSpec {
     implicit val json: JSON[TypeB] = deriveJSON[TypeB]
   }
 }
-*/
