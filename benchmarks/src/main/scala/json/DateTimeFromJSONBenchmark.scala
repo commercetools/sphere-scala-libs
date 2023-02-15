deb40d6f49cbf9c426ec9d9876b4a4fb255fad9b
Johannes Pirkl <johannes.pirkl@commercetools.com>
2023-02-15 12:54:11 +0100

    Remove redundant value from benchmark

--- benchmarks/src/main/scala/json/DateTimeFromJSONBenchmark.scala
+++ benchmarks/src/main/scala/json/DateTimeFromJSONBenchmark.scala
@@ -19,7 +19,6 @@ class DateTimeFromJSONBenchmark {
     Array(
       "2025-12-14T12:50:25.070Z",
       "2022-09-05T00:18:33.994Z",
-      "2022-09-05T00:18:33.994Z"
     ))
   var rawValue: String = _
   private var json: JString = _
