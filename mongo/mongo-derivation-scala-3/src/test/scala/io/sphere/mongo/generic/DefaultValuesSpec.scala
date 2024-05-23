8ced2154fa7276537c661c6b4343fc4b01ed37ba
Peter Empen <peter.empen@commercetools.com>
2024-05-23 12:34:15 +0200

    add DefaultValuesSpec

---
+++ mongo/mongo-derivation-scala-3/src/test/scala/io/sphere/mongo/generic/DefaultValuesSpec.scala
@@ -0,0 +1,31 @@
+package io.sphere.mongo.generic
+
+import io.sphere.mongo.MongoUtils.*
+import io.sphere.mongo.format.DefaultMongoFormats.*
+import io.sphere.mongo.format.MongoFormat
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class DefaultValuesSpec extends AnyWordSpec with Matchers:
+  import DefaultValuesSpec.*
+
+  "deriving MongoFormat" must {
+    "handle default values" in {
+      val dbo = dbObj()
+      val test = MongoFormat[Test].fromMongoValue(dbo)
+      test.value1 mustBe "hello"
+      test.value2 mustBe None
+      test.value3 mustBe None
+      test.value4 mustBe Some("hi")
+    }
+  }
+
+object DefaultValuesSpec:
+  case class Test(
+      value1: String = "hello",
+      value2: Option[String],
+      value3: Option[String] = None,
+      value4: Option[String] = Some("hi")
+  )
+  object Test:
+    implicit val mongo: MongoFormat[Test] = mongoProduct(apply _)
