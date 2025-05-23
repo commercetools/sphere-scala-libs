da63668c283bee59668315d4a400865f18582eb9
Benko Balog <benko.balog@commercetools.com>
2025-05-23 15:48:51 +0200

    Add scala-2 syntax for 'jsonTypeSwitch' methods for scala3

--- json/json-core/src/main/scala-3/io/sphere/json/generic/generic.scala
+++ json/json-core/src/main/scala-3/io/sphere/json/generic/generic.scala
@@ -27,3 +27,68 @@ inline def fromJsonTypeSwitch[SuperType, SubTypes <: Tuple]: FromJSON[SuperType]
   val info = JSONTypeSwitch.readTraitInformation[SuperType, SubTypes]
   JSONTypeSwitch.fromJsonTypeSwitch[SuperType](info)
 }
+
+// Compatibility with the scala-2 methods, that will be deprecated later
+// fromJsonTypeSwitch is not used, so no old syntax support will be added for now
+
+// jsonTypeSwitch is used up to 26 parameters, so I'll up to 28
+// format: off
+inline def jsonTypeSwitch[SuperType, A1: JSON](ignoredList: List[Nothing]): JSON[SuperType] =
+  jsonTypeSwitch[SuperType, Tuple1[A1]]
+inline def jsonTypeSwitch[SuperType, A1: JSON, A2: JSON](ignoredList: List[Nothing]): JSON[SuperType] =
+  jsonTypeSwitch[SuperType, (A1, A2)]
+inline def jsonTypeSwitch[SuperType, A1: JSON, A2: JSON, A3: JSON](ignoredList: List[Nothing]): JSON[SuperType] =
+  jsonTypeSwitch[SuperType, (A1, A2, A3)]
+inline def jsonTypeSwitch[SuperType, A1: JSON, A2: JSON, A3: JSON, A4: JSON](ignoredList: List[Nothing]): JSON[SuperType] =
+  jsonTypeSwitch[SuperType, (A1, A2, A3, A4)]
+inline def jsonTypeSwitch[SuperType, A1: JSON, A2: JSON, A3: JSON, A4: JSON, A5: JSON](ignoredList: List[Nothing]): JSON[SuperType] =
+  jsonTypeSwitch[SuperType, (A1, A2, A3, A4, A5)]
+inline def jsonTypeSwitch[SuperType, A1: JSON, A2: JSON, A3: JSON, A4: JSON, A5: JSON, A6: JSON](ignoredList: List[Nothing]): JSON[SuperType] =
+  jsonTypeSwitch[SuperType, (A1, A2, A3, A4, A5, A6)]
+inline def jsonTypeSwitch[SuperType, A1: JSON, A2: JSON, A3: JSON, A4: JSON, A5: JSON, A6: JSON, A7: JSON](ignoredList: List[Nothing]): JSON[SuperType] =
+  jsonTypeSwitch[SuperType, (A1, A2, A3, A4, A5, A6, A7)]
+inline def jsonTypeSwitch[SuperType, A1: JSON, A2: JSON, A3: JSON, A4: JSON, A5: JSON, A6: JSON, A7: JSON, A8: JSON](ignoredList: List[Nothing]): JSON[SuperType] =
+  jsonTypeSwitch[SuperType, (A1, A2, A3, A4, A5, A6, A7, A8)]
+inline def jsonTypeSwitch[SuperType, A1: JSON, A2: JSON, A3: JSON, A4: JSON, A5: JSON, A6: JSON, A7: JSON, A8: JSON, A9: JSON](ignoredList: List[Nothing]): JSON[SuperType] =
+  jsonTypeSwitch[SuperType, (A1, A2, A3, A4, A5, A6, A7, A8, A9)]
+inline def jsonTypeSwitch[SuperType, A1: JSON, A2: JSON, A3: JSON, A4: JSON, A5: JSON, A6: JSON, A7: JSON, A8: JSON, A9: JSON, A10: JSON](ignoredList: List[Nothing]): JSON[SuperType] =
+  jsonTypeSwitch[SuperType, (A1, A2, A3, A4, A5, A6, A7, A8, A9, A10)]
+inline def jsonTypeSwitch[SuperType, A1: JSON, A2: JSON, A3: JSON, A4: JSON, A5: JSON, A6: JSON, A7: JSON, A8: JSON, A9: JSON, A10: JSON, A11: JSON](ignoredList: List[Nothing]): JSON[SuperType] =
+  jsonTypeSwitch[SuperType, (A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11)]
+inline def jsonTypeSwitch[SuperType, A1: JSON, A2: JSON, A3: JSON, A4: JSON, A5: JSON, A6: JSON, A7: JSON, A8: JSON, A9: JSON, A10: JSON, A11: JSON, A12: JSON](ignoredList: List[Nothing]): JSON[SuperType] =
+  jsonTypeSwitch[SuperType, (A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12)]
+inline def jsonTypeSwitch[SuperType, A1: JSON, A2: JSON, A3: JSON, A4: JSON, A5: JSON, A6: JSON, A7: JSON, A8: JSON, A9: JSON, A10: JSON, A11: JSON, A12: JSON, A13: JSON](ignoredList: List[Nothing]): JSON[SuperType] =
+  jsonTypeSwitch[SuperType, (A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13)]
+inline def jsonTypeSwitch[SuperType, A1: JSON, A2: JSON, A3: JSON, A4: JSON, A5: JSON, A6: JSON, A7: JSON, A8: JSON, A9: JSON, A10: JSON, A11: JSON, A12: JSON, A13: JSON, A14: JSON](ignoredList: List[Nothing]): JSON[SuperType] =
+  jsonTypeSwitch[SuperType, (A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14)]
+inline def jsonTypeSwitch[SuperType, A1: JSON, A2: JSON, A3: JSON, A4: JSON, A5: JSON, A6: JSON, A7: JSON, A8: JSON, A9: JSON, A10: JSON, A11: JSON, A12: JSON, A13: JSON, A14: JSON, A15: JSON](ignoredList: List[Nothing]): JSON[SuperType] =
+  jsonTypeSwitch[SuperType, (A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15)]
+inline def jsonTypeSwitch[SuperType, A1: JSON, A2: JSON, A3: JSON, A4: JSON, A5: JSON, A6: JSON, A7: JSON, A8: JSON, A9: JSON, A10: JSON, A11: JSON, A12: JSON, A13: JSON, A14: JSON, A15: JSON, A16: JSON](ignoredList: List[Nothing]): JSON[SuperType] =
+  jsonTypeSwitch[SuperType, (A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16)]
+inline def jsonTypeSwitch[SuperType, A1: JSON, A2: JSON, A3: JSON, A4: JSON, A5: JSON, A6: JSON, A7: JSON, A8: JSON, A9: JSON, A10: JSON, A11: JSON, A12: JSON, A13: JSON, A14: JSON, A15: JSON, A16: JSON, A17: JSON](ignoredList: List[Nothing]): JSON[SuperType] =
+  jsonTypeSwitch[SuperType, (A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16, A17)]
+inline def jsonTypeSwitch[SuperType, A1: JSON, A2: JSON, A3: JSON, A4: JSON, A5: JSON, A6: JSON, A7: JSON, A8: JSON, A9: JSON, A10: JSON, A11: JSON, A12: JSON, A13: JSON, A14: JSON, A15: JSON, A16: JSON, A17: JSON, A18: JSON](ignoredList: List[Nothing]): JSON[SuperType] =
+  jsonTypeSwitch[SuperType, (A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16, A17, A18)]
+inline def jsonTypeSwitch[SuperType, A1: JSON, A2: JSON, A3: JSON, A4: JSON, A5: JSON, A6: JSON, A7: JSON, A8: JSON, A9: JSON, A10: JSON, A11: JSON, A12: JSON, A13: JSON, A14: JSON, A15: JSON, A16: JSON, A17: JSON, A18: JSON, A19: JSON](ignoredList: List[Nothing]): JSON[SuperType] =
+  jsonTypeSwitch[SuperType, (A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16, A17, A18, A19)]
+inline def jsonTypeSwitch[SuperType, A1: JSON, A2: JSON, A3: JSON, A4: JSON, A5: JSON, A6: JSON, A7: JSON, A8: JSON, A9: JSON, A10: JSON, A11: JSON, A12: JSON, A13: JSON, A14: JSON, A15: JSON, A16: JSON, A17: JSON, A18: JSON, A19: JSON, A20: JSON](ignoredList: List[Nothing]): JSON[SuperType] =
+  jsonTypeSwitch[SuperType, (A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16, A17, A18, A19, A20)]
+inline def jsonTypeSwitch[SuperType, A1: JSON, A2: JSON, A3: JSON, A4: JSON, A5: JSON, A6: JSON, A7: JSON, A8: JSON, A9: JSON, A10: JSON, A11: JSON, A12: JSON, A13: JSON, A14: JSON, A15: JSON, A16: JSON, A17: JSON, A18: JSON, A19: JSON, A20: JSON, A21: JSON](ignoredList: List[Nothing]): JSON[SuperType] =
+  jsonTypeSwitch[SuperType, (A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16, A17, A18, A19, A20, A21)]
+inline def jsonTypeSwitch[SuperType, A1: JSON, A2: JSON, A3: JSON, A4: JSON, A5: JSON, A6: JSON, A7: JSON, A8: JSON, A9: JSON, A10: JSON, A11: JSON, A12: JSON, A13: JSON, A14: JSON, A15: JSON, A16: JSON, A17: JSON, A18: JSON, A19: JSON, A20: JSON, A21: JSON, A22: JSON](ignoredList: List[Nothing]): JSON[SuperType] =
+  jsonTypeSwitch[SuperType, (A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16, A17, A18, A19, A20, A21, A22)]
+inline def jsonTypeSwitch[SuperType, A1: JSON, A2: JSON, A3: JSON, A4: JSON, A5: JSON, A6: JSON, A7: JSON, A8: JSON, A9: JSON, A10: JSON, A11: JSON, A12: JSON, A13: JSON, A14: JSON, A15: JSON, A16: JSON, A17: JSON, A18: JSON, A19: JSON, A20: JSON, A21: JSON, A22: JSON, A23: JSON](ignoredList: List[Nothing]): JSON[SuperType] =
+  jsonTypeSwitch[SuperType, (A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16, A17, A18, A19, A20, A21, A22, A23)]
+inline def jsonTypeSwitch[SuperType, A1: JSON, A2: JSON, A3: JSON, A4: JSON, A5: JSON, A6: JSON, A7: JSON, A8: JSON, A9: JSON, A10: JSON, A11: JSON, A12: JSON, A13: JSON, A14: JSON, A15: JSON, A16: JSON, A17: JSON, A18: JSON, A19: JSON, A20: JSON, A21: JSON, A22: JSON, A23: JSON, A24: JSON](ignoredList: List[Nothing]): JSON[SuperType] =
+  jsonTypeSwitch[SuperType, (A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16, A17, A18, A19, A20, A21, A22, A23, A24)]
+inline def jsonTypeSwitch[SuperType, A1: JSON, A2: JSON, A3: JSON, A4: JSON, A5: JSON, A6: JSON, A7: JSON, A8: JSON, A9: JSON, A10: JSON, A11: JSON, A12: JSON, A13: JSON, A14: JSON, A15: JSON, A16: JSON, A17: JSON, A18: JSON, A19: JSON, A20: JSON, A21: JSON, A22: JSON, A23: JSON, A24: JSON, A25: JSON](ignoredList: List[Nothing]): JSON[SuperType] =
+  jsonTypeSwitch[SuperType, (A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16, A17, A18, A19, A20, A21, A22, A23, A24, A25)]
+inline def jsonTypeSwitch[SuperType, A1: JSON, A2: JSON, A3: JSON, A4: JSON, A5: JSON, A6: JSON, A7: JSON, A8: JSON, A9: JSON, A10: JSON, A11: JSON, A12: JSON, A13: JSON, A14: JSON, A15: JSON, A16: JSON, A17: JSON, A18: JSON, A19: JSON, A20: JSON, A21: JSON, A22: JSON, A23: JSON, A24: JSON, A25: JSON, A26: JSON](ignoredList: List[Nothing]): JSON[SuperType] =
+  jsonTypeSwitch[SuperType, (A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16, A17, A18, A19, A20, A21, A22, A23, A24, A25, A26)]
+inline def jsonTypeSwitch[SuperType, A1: JSON, A2: JSON, A3: JSON, A4: JSON, A5: JSON, A6: JSON, A7: JSON, A8: JSON, A9: JSON, A10: JSON, A11: JSON, A12: JSON, A13: JSON, A14: JSON, A15: JSON, A16: JSON, A17: JSON, A18: JSON, A19: JSON, A20: JSON, A21: JSON, A22: JSON, A23: JSON, A24: JSON, A25: JSON, A26: JSON, A27: JSON](ignoredList: List[Nothing]): JSON[SuperType] =
+  jsonTypeSwitch[SuperType, (A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16, A17, A18, A19, A20, A21, A22, A23, A24, A25, A26, A27)]
+inline def jsonTypeSwitch[SuperType, A1: JSON, A2: JSON, A3: JSON, A4: JSON, A5: JSON, A6: JSON, A7: JSON, A8: JSON, A9: JSON, A10: JSON, A11: JSON, A12: JSON, A13: JSON, A14: JSON, A15: JSON, A16: JSON, A17: JSON, A18: JSON, A19: JSON, A20: JSON, A21: JSON, A22: JSON, A23: JSON, A24: JSON, A25: JSON, A26: JSON, A27: JSON, A28: JSON](ignoredList: List[Nothing]): JSON[SuperType] =
+  jsonTypeSwitch[SuperType, (A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16, A17, A18, A19, A20, A21, A22, A23, A24, A25, A26, A27, A28)]
+// format: on
+
+// toJsonTypeSwitch is used up to 5 parameters, so I'll add up to 7
--- json/json-derivation/src/test/scala-3/io/sphere/json/generic/JsonTypeSwitchSpec.scala
+++ json/json-derivation/src/test/scala-3/io/sphere/json/generic/JsonTypeSwitchSpec.scala
@@ -10,49 +10,55 @@ import org.json4s.*
 import org.json4s.DefaultReaders.StringReader
 
 class JsonTypeSwitchSpec extends AnyWordSpec with Matchers {
+  import JsonTypeSwitchSpec.*
 
   "jsonTypeSwitch" must {
-    import JsonTypeSwitchSpec.*
 
-    "derive a subset of a sealed trait" in {
+    {
       given JSON[B] = deriveJSON[B]
-      val format = jsonTypeSwitch[A, (B, C)]
+      "derive a subset of a sealed trait".withFormatters(
+        newSyntax = jsonTypeSwitch[A, (B, C)],
+        oldSyntax = jsonTypeSwitch[A, B, C](Nil)
+      ) { format =>
+        val b = B(123)
+        val jsonB = format.write(b)
 
-      val b = B(123)
-      val jsonB = format.write(b)
+        val b2 = format.read(jsonB).getOrElse(null)
 
-      val b2 = format.read(jsonB).getOrElse(null)
+        b2 must be(b)
 
-      b2 must be(b)
+        val c = C(2345345)
+        val jsonC = format.write(c)
 
-      val c = C(2345345)
-      val jsonC = format.write(c)
+        val c2 = format.read(jsonC).getOrElse(null)
 
-      val c2 = format.read(jsonC).getOrElse(null)
-
-      c2 must be(c)
+        c2 must be(c)
+      }
     }
 
-    "derive a subset of a sealed trait with a mongoKey" in {
-      val format = jsonTypeSwitch[A, (B, D)]
-
+    "derive a subset of a sealed trait with a mongoKey".withFormatters(
+      newSyntax = jsonTypeSwitch[A, (B, D)],
+      oldSyntax = jsonTypeSwitch[A, B, D](Nil)
+    ) { format =>
       val d = D(123)
       val json = format.write(d)
       val d2 = format.read(json)
 
       (json \ "type").as[String] must be("D2")
       d2 must be(Valid(d))
-
     }
 
-    "combine different sum types tree" in {
+    "combine different sum types tree".withFormatters(
+      newSyntax = jsonTypeSwitch[Message, (TypeA, TypeB)],
+      oldSyntax = jsonTypeSwitch[Message, (TypeA, TypeB)]
+    ) { format =>
       val m: Seq[Message] = List(
         TypeA.ClassA1(23),
         TypeA.ClassA2("world"),
         TypeB.ClassB1(valid = false),
         TypeB.ClassB2(Seq("a23", "c62")))
 
-      val jsons = m.map(Message.json.write)
+      val jsons = m.map(format.write)
       jsons must be(
         List(
           JObject("number" -> JLong(23), "type" -> JString("ClassA1")),
@@ -63,34 +69,45 @@ class JsonTypeSwitchSpec extends AnyWordSpec with Matchers {
             "type" -> JString("ClassB2"))
         ))
 
-      val messages = jsons.map(Message.json.read).map(_.toOption.get)
+      val messages = jsons.map(format.read).map(_.toOption.get)
       messages must be(m)
     }
 
-    "handle custom implementations for subtypes" in {
-
+    {
       given JSON[B] = new JSON[B] {
         override def read(jval: JValue): JValidation[B] = jval match {
           case JObject(List(_, "field" -> JString(s"Custom-B-${n}"))) =>
             Valid(B(n.toInt))
           case _ => ???
         }
+
         override def write(value: B): JValue =
           JObject(List("field" -> JString(s"Custom-B-${value.int}")))
       }
-      val format = jsonTypeSwitch[A, (B, D, C)]
 
-      List(
-        D(2345),
-        C(4),
-        B(34)
-      ).foreach { value =>
-        val json = format.write(value)
-        format.read(json).getOrElse(null) must be(value)
+      "handle custom implementations for subtypes".withFormatters(
+        newSyntax = jsonTypeSwitch[A, (B, D, C)],
+        oldSyntax = jsonTypeSwitch[A, B, D, C](Nil)
+      ) { format =>
+        List(D(2345), C(4), B(34)).foreach { value =>
+          val json = format.write(value)
+          format.read(json).getOrElse(null) must be(value)
+        }
       }
     }
   }
 
+  extension (string: String) {
+    def withFormatters[A](newSyntax: JSON[A], oldSyntax: JSON[A])(f: JSON[A] => Any): Unit = {
+      s"$string with newSyntax" in {
+        f(newSyntax)
+      }
+
+      s"$string with oldSyntax" in {
+        f(oldSyntax)
+      }
+    }
+  }
 }
 
 object JsonTypeSwitchSpec {
@@ -100,12 +117,6 @@ object JsonTypeSwitchSpec {
   @JSONTypeHint("D2") case class D(int: Int) extends A
 
   trait Message
-  object Message {
-    // this can be dangerous is the same class name is used in both sum types
-    // ex if we define TypeA.Class1 && TypeB.Class1
-    // as both will use the same type value discriminator
-    implicit val json: JSON[Message] = jsonTypeSwitch[Message, (TypeA, TypeB)]
-  }
 
   sealed trait TypeA extends Message
   object TypeA {
