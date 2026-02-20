4183dfb01f49308af2ac00b5c07ad1dfc37291d6
benkobalog <benko.balog@commercetools.com>
2026-02-20 13:17:07 +0100

    fmt

--- util/src/main/scala/Money.scala
+++ util/src/main/scala/Money.scala
@@ -60,7 +60,9 @@ object BaseMoney {
   val TypeField: String = "type"
 
   def requireSameCurrency(m1: BaseMoney, m2: BaseMoney): Unit =
-    require(m1.currency == m2.currency, s"${m1.currency.uniqueCurrencyCode} != ${m2.currency.uniqueCurrencyCode}")
+    require(
+      m1.currency == m2.currency,
+      s"${m1.currency.uniqueCurrencyCode} != ${m2.currency.uniqueCurrencyCode}")
 
   def toScalaRoundingMode(mode: java.math.RoundingMode): RoundingMode =
     BigDecimal.RoundingMode(mode.ordinal())
