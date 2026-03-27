5846a817e15c40c884ebef7d9ed9fff94c45fc38
benkobalog <benko.balog@commercetools.com>
2026-03-27 16:34:53 +0100

    [Scala3 backporting] MongoIgnoreSpec

--- mongo/mongo-derivation/src/test/scala/io/sphere/mongo/generic/MongoIgnoreSpec.scala
+++ mongo/mongo-derivation/src/test/scala/io/sphere/mongo/generic/MongoIgnoreSpec.scala
@@ -8,8 +8,6 @@ import org.scalatest.matchers.must.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
 object MongoIgnoreSpec {
-  private val aName = "aName"
-
   private case class MissingDefault(name: String, @MongoIgnore age: Int)
 
   private case class Complete(name: String, @MongoIgnore age: Int = 100)
@@ -21,13 +19,13 @@ class MongoIgnoreSpec extends AnyWordSpec with Matchers with OptionValues {
   "MongoIgnore" when {
     "annotated field has no default" must {
       "fail with a suitable message" in {
-        val format = deriveMongoFormat[MissingDefault]
-        val e = the[Exception] thrownBy format.fromMongoValue(dbObj("name" -> aName))
+        val e = the[Exception] thrownBy deriveMongoFormat[MissingDefault]
         e.getMessage mustBe "Ignored Mongo field 'age' must have a default value."
       }
     }
     "annotated field has a default value" must {
       "omit the field from the BSON" in {
+        val aName = "aName"
         val format = deriveMongoFormat[Complete]
         format.toMongoValue(Complete(aName)) mustBe dbObj("name" -> aName)
       }
