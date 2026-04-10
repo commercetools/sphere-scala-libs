45f7a819fa235ca558741dd54118896db1e1d925
benkobalog <benko.balog@commercetools.com>
2026-04-10 17:52:40 +0200

    Fix test names in JsonTypeHintFieldSpec

--- json/json-derivation/src/test/scala/io/sphere/json/generic/JsonTypeHintFieldSpec.scala
+++ json/json-derivation/src/test/scala/io/sphere/json/generic/JsonTypeHintFieldSpec.scala
@@ -10,7 +10,7 @@ class JsonTypeHintFieldSpec extends AnyWordSpec with Matchers {
   import JsonTypeHintFieldSpec._
 
   "JSONTypeHintField" must {
-    "allow to set another field to distinguish between types (toMongo)" in {
+    "allow to set another field to distinguish between types (toJSON)" in {
       val user = UserWithPicture("foo-123", Medium, "http://example.com")
       val expected = JObject(
         List(
@@ -22,7 +22,7 @@ class JsonTypeHintFieldSpec extends AnyWordSpec with Matchers {
       json must be(expected)
     }
 
-    "allow to set another field to distinguish between types (fromMongo)" in {
+    "allow to set another field to distinguish between types (fromJSON)" in {
       val json =
         """
         {
