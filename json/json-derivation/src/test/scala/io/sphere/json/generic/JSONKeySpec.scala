77cd1b155a070c28b969c53b0a6049d1ee540ef0
benkobalog <benko.balog@commercetools.com>
2026-07-27 14:54:28 +0200

    Backport tests from Scala3 branch

--- json/json-derivation/src/test/scala/io/sphere/json/generic/JSONKeySpec.scala
+++ json/json-derivation/src/test/scala/io/sphere/json/generic/JSONKeySpec.scala
@@ -2,6 +2,8 @@ package io.sphere.json.generic
 
 import io.sphere.json._
 import org.json4s.DefaultReaders._
+import org.json4s.MonadicJValue.jvalueToMonadic
+import org.json4s.jvalue2readerSyntax
 import org.scalatest.matchers.must.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
@@ -21,6 +23,12 @@ class JSONKeySpec extends AnyWordSpec with Matchers {
 
       val newTest = getFromJValue[Test](json)
       newTest must be(test)
+    }
+
+    "decode an Option of a case class that has a renamed field (@JSONKey)" in {
+      val sub = SubTest(value2 = "other_value2")
+      val json = toJValue[Option[SubTest]](Some(sub))
+      getFromJValue[Option[SubTest]](json) must be(Some(sub))
     }
   }
 }
--- mongo/mongo-derivation/src/test/scala/io/sphere/mongo/generic/MongoKeySpec.scala
+++ mongo/mongo-derivation/src/test/scala/io/sphere/mongo/generic/MongoKeySpec.scala
@@ -6,7 +6,7 @@ import org.bson.BSONObject
 import org.scalatest.matchers.must.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
-import scala.collection.JavaConverters._
+import scala.jdk.CollectionConverters._
 
 class MongoKeySpec extends AnyWordSpec with Matchers {
   import MongoKeySpec._
@@ -25,6 +25,12 @@ class MongoKeySpec extends AnyWordSpec with Matchers {
 
       val newTest = MongoFormat[Test].fromMongoValue(dbo)
       newTest must be(test)
+    }
+
+    "decode an Option of a case class that has a renamed field (@MongoKey)" in {
+      val sub = SubTest(value2 = "other_value2")
+      val bson = MongoFormat[Option[SubTest]].toMongoValue(Some(sub))
+      MongoFormat[Option[SubTest]].fromMongoValue(bson) must be(Some(sub))
     }
   }
 }
