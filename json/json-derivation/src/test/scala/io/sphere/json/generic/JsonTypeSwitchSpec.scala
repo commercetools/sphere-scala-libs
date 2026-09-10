7f85d3cfd0f7396416d4241641f87008a7518cd2
benkobalog <benko.balog@commercetools.com>
2026-09-10 13:36:24 +0200

    Merge JsonTypeSwitchSpecCommon.scala

--- json/json-derivation/src/test/scala/io/sphere/json/generic/JsonTypeSwitchSpec.scala
+++ json/json-derivation/src/test/scala/io/sphere/json/generic/JsonTypeSwitchSpec.scala
@@ -1,16 +1,16 @@
 package io.sphere.json.generic
 
 import cats.data.Validated.Valid
-import cats.syntax.traverse.*
+import cats.syntax.traverse._
 import io.sphere.json.{JSON, JValidation, parseJSON}
-import io.sphere.util.test.*
-import org.json4s.*
+import io.sphere.util.test._
+import org.json4s._
 import org.json4s.DefaultReaders.StringReader
 import org.scalatest.matchers.must.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
 class JsonTypeSwitchSpec extends AnyWordSpec with Matchers {
-  import JsonTypeSwitchModels.*
+  import JsonTypeSwitchModels._
 
   "jsonTypeSwitch" must {
 
